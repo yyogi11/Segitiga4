9b428355787f0417f2165b2ad71297082d65c951
yyogi11 <yogidapa@webmail.umm.ac.id>
2023-11-01 23:00:39 +0700

    first Commit

---
+++ src/main/java/org/example/LuasSegitiga.java
@@ -0,0 +1,27 @@
+package org.example;
+import java.util.Scanner;
+
+/**
+ * Program sederhana untuk menghitung luas segitiga berdasarkan panjang alas dan tingginya.
+ */
+public class LuasSegitiga {
+
+    /**
+     * Metode utama untuk menghitung luas segitiga.
+     *
+     * @param args Argumen baris perintah. Tidak digunakan dalam program ini.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double alas, tinggi;
+        System.out.print("Masukkan panjang alas segitiga: ");
+        alas = scanner.nextDouble();
+
+        System.out.print("Masukkan tinggi segitiga: ");
+        tinggi = scanner.nextDouble();
+
+        double luas = (alas * tinggi) / 2;
+
+        System.out.println("Luas segitiga adalah: " + luas);
+    }
+}
